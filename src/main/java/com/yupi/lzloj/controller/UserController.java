b8a13461b72a23be93e49127b94007e2e24621af
kukudelong <kukudelongoflzl@gmail.com>
2024-08-26 11:06:10 +0800

    系统的健壮性

--- src/main/java/com/yupi/lzloj/controller/UserController.java
+++ src/main/java/com/yupi/lzloj/controller/UserController.java
@@ -45,8 +45,8 @@ import static com.yupi.lzloj.service.impl.UserServiceImpl.SALT;
 /**
  * 用户接口
  *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ * 
+ * 提交
  */
 @RestController
 @RequestMapping("/user")
